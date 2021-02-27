6991e5a941db767058add7ce9e7a0f8bb3effc3a
k2 <trofimchuk.an@gmail.com>
2021-02-27 23:34:58 +0200

    Start

---
+++ Lessons/01. DataTypes/01. DataTypes/01. DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            if (name == "")
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else
+            {
+                Console.WriteLine("Hello, " + name + "!");
+            }
+        }
+    }
+}
