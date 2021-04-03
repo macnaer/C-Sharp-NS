732974c3822074e960cafaaa881aa1d73a779cd1
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2021-04-03 20:07:26 +0300

    Int Extentions

---
+++ Lessons/05. Успадкування. Поліморфізм. Абстрактні  класи/Ext_methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ext_methods
+{
+    // extention methods 
+    // 1. in static class
+    // 
+
+    static class IntExtention
+    {
+        public static int CountDigits(this int number)
+        {
+            return Math.Abs(number).ToString().Length;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IntExtention.CountDigits(123));
+            int value = -3456;
+            Console.WriteLine(value.CountDigits());
+        }
+    }
+}
